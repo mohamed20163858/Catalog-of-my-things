29dd96106d02dd328e93ace6c85730506d9dd365
sonickmumba <kingellie.mumba@gmail.com>
2022-12-08 23:22:27 +0200

    Implement can_be_archived method

--- game.rb
+++ game.rb
@@ -5,19 +5,22 @@ class Game < Item
   attr_reader :name
 
   def initialize(name, multiplayer, last_played_at, publish_date, archived)
-    super(id, archived, publish_date)
+    super(id, publish_date, archived)
     @multiplayer = multiplayer
     @last_played_at = last_played_at
-    @publish_date = publish_date
     @name = name
     @id = Random.rand(1..1000)
   end
 
   private
 
-  def can_be_archived?
-    current_year = Time.new.year
-    remaining_year = current_year - last_played_at
+  def can_be_archieved?
+    (year, month, day) = @last_played_at.split('-')
+    year = year.to_i
+    month = month.to_i
+    day = day.to_i
+    date = Time.new(year, month, day)
+    remaining_year = (Time.now - date) / (365.25 * 24 * 60 * 60)
     super() && (remaining_year > 2)
   end
 end
