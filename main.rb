cda7b4f7ce8212a035d9d8ee6e1e6bb98b90a895
OmarMWarraich <earthling986@gmail.com>
2022-12-09 23:05:29 +0500

    feat: Add Unit Tests

--- main.rb
+++ main.rb
@@ -2,6 +2,12 @@ require_relative './option'
 require_relative './save'
 require_relative './load'
 require_relative './menu'
+require_relative './music_album'
+require_relative './genre'
+
+# rubocop:disable Metrics/MethodLength
+# rubocop:disable Metrics/CyclomaticComplexity
+
 def main
   number = 0
   menu = Menu.new
@@ -14,6 +20,9 @@ def main
   option.app.labels = File.exist?('labels.json') ? l.load_labels : []
   option.app.games = File.exist?('games.json') ? l.load_games : []
   option.app.authors = File.exist?('authors.json') ? l.load_authors : []
+  option.app.music_albums = File.exist?('music_albums.json') ? l.load_music_albums : []
+  option.app.genres = File.exist?('genres.json') ? l.load_genres : []
+
   while number != '17'
     menu.display_options
     number = gets.chomp
@@ -24,6 +33,11 @@ def main
   s.save_labels(option.app.labels)
   s.save_games(option.app.games)
   s.save_authors(option.app.authors)
+  s.save_music_albums(option.app.music_albums)
+  s.save_genres(option.app.genres)
 end
+
+# rubocop:enable Metrics/MethodLength
+# rubocop:enable Metrics/CyclomaticComplexity
 
 main
---
+++ spec/music_album_spec.rb
@@ -0,0 +1,61 @@
+require_relative '../music_album'
+
+describe MusicAlbum do
+    context 'test MusicAlbum class behaviour' do
+        before(:all) do
+        @id = 1
+        @name = 'The Dark Side of the Moon'
+        @publish_date = '2020-09-01'
+        @artist = 'Omar'
+        @genre = 'Rock'
+        @archived = true
+        @on_spotify = true
+        @music_album = MusicAlbum.new(@id, @name, @artist, @genre, @publish_date, @archived, @on_spotify)
+        end
+        it 'test the existence of music_album class' do
+        expect(@music_album).to be_instance_of(MusicAlbum) # assert
+        end
+        it 'test music_album is class is a child of item class' do
+        expect(@music_album).to be_kind_of(Item) # assert
+        end
+    end
+end
+
+describe MusicAlbum do
+    context 'test music_album move_to_archive method  behaviour' do
+        before(:all) do
+        @id = 1
+        @name = 'The Dark Side of the Moon'
+        @artist = 'Omar'
+        @genre = 'Rock'
+        @archived = false
+        end
+        it 'test the move_to_archive by giving an input the age of it < 10 years
+        and archived = false and on_spotify = true ' do
+        # arrange
+        publish_date = '2013-12-13'
+        on_spotify = true
+        music_album = MusicAlbum.new(@id, @name, @artist, @genre, publish_date, @archived, on_spotify)
+        result = music_album.move_to_archive
+        expect(result).to be nil
+        end
+        it 'test the move_to_archive by giving an input the age of it > 10 years
+        and archived = false and on_spotify = true ' do
+        # arrange
+        publish_date = '2010-12-13'
+        on_spotify = true
+        music_album = MusicAlbum.new(@id, @name, @artist, @genre, publish_date, @archived, on_spotify)
+        result = music_album.move_to_archive
+        expect(result).to be true
+        end
+        it 'test the move_to_archive by giving an input the age of it < 10 years
+        and archived = false and on_spotify = false ' do
+        # arrange
+        publish_date = '2013-12-13'
+        on_spotify = false
+        music_album = MusicAlbum.new(@id, @name, @artist, @genre, publish_date, @archived, on_spotify)
+        result = music_album.move_to_archive
+        expect(result).to be nil
+        end
+    end
+end
