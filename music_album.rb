167a713be0801a62042be5f0310aadbea1139d7b
OmarMWarraich <earthling986@gmail.com>
2022-12-09 17:09:32 +0500

    feat: Add MusicAlbum class and can_be_archived method to it

---
+++ music_album.rb
@@ -0,0 +1,22 @@
+require './item'
+
+class MusicAlbum < Item
+
+    attr_accessor :artist, :genre, :id, :on_spotify
+    attr_reader :name
+    
+    def initialize(name, artist, genre, publish_date, archived, on_spotify)
+        super(id, publish_date, archived)
+        @artist = artist
+        @genre = genre
+        @name = name
+        @id = Random.rand(1..1000)
+        @on_spotify = on_spotify
+    end
+    
+    private
+    
+    def can_be_archieved?
+        super() && @on_spotify
+    end
+    end
